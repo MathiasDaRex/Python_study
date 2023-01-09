a9bc253e81c1dee3ea2f7a08b49e3b364c4c39e5
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2023-01-09 13:33:20 +0100

    Update 16_2D-lists.py

--- 16_2D-lists.py
+++ 16_2D-lists.py
@@ -1,9 +1,10 @@
 # 2D lists = a list of lists
 
-drinks = ["cola","coffee","tea"]
-dinner = ["pizza","spagetti","hamburger"]
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
 dessert = ["cake","ice cream"]
 
 food = [drinks, dinner, dessert]
+print(food)
 
-print(food[2][1])
+print("Menu 1: "+food[1][1]+" and "+food[2][0]+" with "+food[0][0])
