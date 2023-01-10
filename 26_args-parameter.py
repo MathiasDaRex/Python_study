b4e60d9d9353751ce07c33e3b9e6e40bad79984d
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2023-01-10 09:21:06 +0100

    Update 26_args-parameter.py

--- 26_args-parameter.py
+++ 26_args-parameter.py
@@ -1,31 +1,34 @@
-# *args = 	parameter that will pack all arguments into a tuple (tuples are ordered and unchangeable)
-# 			useful so that a function cann accept a varying amount of arguments
+# *args = 	parameter that will pack all arguments into a tuple
+#			useful so that a function can accept a varying amount of arguments
 
+# tuples - ORDERED and UNCHANGEABLE
 
-# With this method we can only pass the predefined amount of arguments
-#def add(x1,x2):
-#    sum = x1 + x2
-#    return sum
-
-#print(add(2,3))
-
-# with *args we can pass as many as we want
-# we don'n have to name it args neccessarily, the most important thing is the  *  before it
-
-# def add(*args):
-#     sum = 0
-#     for i in args:
-#          sum += i
+# defining 2 arguments, we must pass 2
+# def add(num1, num2):
+#     sum = num1 + num2
 #     return sum
 
-def add(*stuff):
+# passing ALL the arguments, and packing them in a tuple
+def add(*args):
     sum = 0
-#    stuff[1] = 1 			  tuples are unchangeable 	to change it's values we have to cast it in some other collection, like a list
-    stuff = list(stuff) 	# casting the stuff tuple to list
-    stuff[4] = 0 			# now we can change the values
-    for i in stuff:
-         sum += i
+    for i in args:
+        sum += i
+    return sum
+
+# we can name *args as we want, the important thing is to have the asterisk (*) before it
+def minus(*numberz):
+    sum = 0
+    # cuz it's a tuple, you cant change it's values
+    # numberz[0] = 0
+    
+    # if we want to change it, we can convert it to a different collection
+    # like a list, what's MUTABLE, and INTERCHANGEABLE
+    numberz = list(numberz)
+    numberz[0] = 0
+    for i in numberz:
+        sum -= i
     return sum
 
 
-print(add(1,2,3,4,5,6,7,8,9,10))
+print(add(1,2,3,4,5))
+print(minus(5,10,20,5))
