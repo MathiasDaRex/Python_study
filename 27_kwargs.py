c4e534c3f247dcc31b832f243d693c2c5d9e8624
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2023-01-10 09:35:56 +0100

    Update 27_kwargs.py

--- 27_kwargs.py
+++ 27_kwargs.py
@@ -1,22 +1,25 @@
 # **kwargs = 	parameter that will pack all arguments into a dictionary
 #				useful so that a function can accept a varying amount of keyword arguments
 
-# if we define how many arguments a method can accept, we cannot add more
+
 
 # def hello(first, last):
 #     print("Hello " + first + " " + last)
 #     
-# hello(first="Mathias", middle="Da" last="Rex")
-
-# if we use **kwargs keyword we can add as many as we want
-# to access the values, we have to call the key for that value from the dictionary
-
-# to iterate through the values, we can use a for loop
+# hello(first="Mathias", middle = "Da", last="Rex")
+# this won't work because we haven't added middle argument to the function
 
 def hello(**kwargs):
-    #print("Hello " + kwargs["first"] + " " + kwargs["last"])
-    print("Hello",end=" ")
-    for key,value in kwargs.items():
-        print(value,end=" ")
+    print("Hello " + kwargs['first'] + " " + kwargs['last'])
     
-hello(first="Mathias", middle="Da", last="Rex", afterlast="DUUDE", lastlastlast="THIS IS ADDED AFTER")
+hello(first="Mathias", middle = "Da", last="Rex")
+
+# if we want to display someones full name based on the amount of keyword arguments they passed
+# like *args we can also name this as we like, the important thing is the ** before it
+
+def hello_var(**names):
+    print("Hello",end=" ")
+    for key,value in names.items():
+        print(value, end=" ")
+    
+hello_var(pre="Mr.",first="Mathias", middle = "Da", last="Rex", lastlast="Dude")
